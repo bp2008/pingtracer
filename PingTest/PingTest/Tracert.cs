3e70b9da193ba68498563d5287799c848bf79d63
bp2008_cp <SND\bp2008_cp>
2015-02-12 22:01:53 +0000

    Refactored to be buildable .NET 3.5 instead of 4.5.  Fixed bug where clicking the graph area before there were any graphs in it led to an uncaught exception.

--- PingTest/PingTest/Tracert.cs
+++ PingTest/PingTest/Tracert.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PingTest
 {
