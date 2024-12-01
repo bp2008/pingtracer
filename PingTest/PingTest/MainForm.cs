c561ca7477faaa0e3852809fcd906b57e21b341a
Dimitri Pappas <fragtion@gmail.com>
2024-12-01 18:03:07 +0200

    Update MainForm.cs

--- PingTest/PingTest/MainForm.cs
+++ PingTest/PingTest/MainForm.cs
@@ -1287,6 +1287,10 @@ namespace PingTracer
 			p.drawPacketLoss = cbPacketLoss.Checked;
 			p.badThreshold = (int)nudBadThreshold.Value;
 			p.worseThreshold = (int)nudWorseThreshold.Value;
+			p.upperLimit = (int)nudUpLimit;
+			p.lowerLimit = (int)nudLowLimit;
+			p.autoScale = cbAutoScale;
+			p.autoScaleLimit = cbAutoScaleLimit;
 			p.preferIpv4 = cbPreferIpv4.Checked;
 			p.logFailures = LogFailures;
 			p.logSuccesses = LogSuccesses;
